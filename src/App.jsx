1bbf648cb0ce3233d449bee218817b1f35ae5703
PratyushSharma <psvt2007@gmail.com>
2025-08-11 19:52:40 +0530

    megablog

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import conf from './conf/conf'
 function App() {
   const [count, setCount] = useState(0)
-
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  // console.log(conf.appwritebucketid)
+  // console.log(conf.appwritecollectionid)
+  // console.log(conf.appwritedatabaseid)
+  // console.log(conf.appwriteprojectid)
+  // console.log(conf.appwriteurl)
   return (<>
   <h1>appwrite</h1>
   </> )
