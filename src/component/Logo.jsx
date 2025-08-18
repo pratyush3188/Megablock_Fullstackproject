0ae7a5178de528a89c77161f3f54056e6027e69d
PratyushSharma <psvt2007@gmail.com>
2025-08-18 18:58:32 +0530

    Logo

---
+++ src/component/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import mylogo from 'Logo.png'
+function Logo({width='100px'}) {
+    return ( <>
+    <img src={mylogo} alt="logo" />    
+    </> );
+}
+
+export default Logo;
